2d22c7e7dca1b8cef0e4ddeccecf9daaa54311cc
Shivani Thakur <shivani7335@gmail.com>
2026-08-23 21:25:51 +0530

    removed Cs from main

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://shivani7335_db_user:TbMfWoYrwVH5UxU4@cluster0.aabvnmo.mongodb.net/?appName=Cluster0');
+    await mongoose.connect('mongodb+srv://<username>:<password>@cluster0.xxxxx.mongodb.net/task_tracking');
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
